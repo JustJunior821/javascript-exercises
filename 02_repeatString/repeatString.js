327d07d0bef2f39fb1d5a9440000b9b4e4c41cf8
JustJunior82 <juniornguyen82@gmail.com>
2023-11-26 13:16:57 -0800

    Completed the 2nd JS exercise
    used JS for loops, function syntax, and concatenating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        return "ERROR";
+    let initial = "";
+    for (let i = 0; i < num; i++)
+    {
+        initial = initial.concat(string);
+    }
+    return initial;
 };
 
 // Do not edit below this line

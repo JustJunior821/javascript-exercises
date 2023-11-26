49e0e410de805412ac172ce52f28ab244779dc43
JustJunior82 <juniornguyen82@gmail.com>
2023-11-26 13:30:11 -0800

    Completed 3rd JS exercise
    learned that array methods have more possibilites than string methods: can convert between them

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(forward) {
+    let reversed = "";
+    let length = forward.length - 1;
+    for (let i = length; i >= 0; i--)
+    {
+        reversed = reversed.concat(forward[i]);
+    }
+    return reversed;
 };
 
 // Do not edit below this line

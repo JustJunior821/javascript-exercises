b2c6aa41282aa9b55d6ea45c25a68baca4d512a2
JustJunior82 <juniornguyen82@gmail.com>
2023-11-26 15:23:30 -0800

    Completed the 4th JS exercise
    used multiple nested for loops to complete: utilize JS array/string methods in future

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let size = args[0].length;
+    for (let i = 1; i < args.length; i++)
+    {
+        for (let j = 0; j < size; j++)
+        {
+            if (args[i] === (args[0])[j])
+                {
+                    for (let k = j + 1; k < size; k++)
+                    {
+                        (args[0])[k-1] = (args[0])[k];
+                    }
+                    size--;
+                    args[0].pop();
+                }
+        }
+    }
+    return args[0];
 
 };
 

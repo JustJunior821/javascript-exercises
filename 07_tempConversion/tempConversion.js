133dbd26e9a9a62c618a013b0c5f64047b4d2d70
JustJunior82 <juniornguyen82@gmail.com>
2023-11-26 19:49:38 -0800

    Completed the 7th JS exercise
    used Math methods to round numbers to certain decimals places

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f_heit) {
+  let celsius = (f_heit - 32) * (5/9) * 10;
+  let convert = Math.round(celsius)/10;
+  return convert;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let f_heit = (celsius * (9/5) + 32) * 10;
+  let convert = Math.round(f_heit)/10;
+  return convert;
 };
 
 // Do not edit below this line

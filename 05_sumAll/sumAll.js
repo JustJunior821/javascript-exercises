75965b28222b1f49de6bad7f107ad283135edf98
JustJunior82 <juniornguyen82@gmail.com>
2023-11-26 16:26:25 -0800

    Completed the 5th JS exercise
    used number methods to check if a variable is an int: review swapping values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let total = 0;
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second))
+        return "ERROR";
+    else if (first < second)
+    {
+        for (let i = first; i <= second; i++)
+        {
+            total += i;
+        }
+        return total;
+    }
+    else
+    {
+        for (let i = second; i <= first; i++)
+        {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
